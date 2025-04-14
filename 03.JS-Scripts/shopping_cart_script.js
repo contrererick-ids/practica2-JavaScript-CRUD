d9fa8fe3a7123945121990ab1001b48cd74372f2
contrererick-ids <is722185@iteso.mx>
2025-04-14 13:32:06 -0600

    feat(cart): Add increaseItemQuantity and decreaseItemQuantity functions
    Implemented functions to update product quantities in the shopping cart.
    Allows users to increment or decrement the quantity of items directly in the cart interface.

--- 03.JS-Scripts/shopping_cart_script.js
+++ 03.JS-Scripts/shopping_cart_script.js
@@ -35,8 +35,8 @@ export class ShoppingCartItem {
                             <p class="cart-item-text">Subtotal: ${this.price * this.quantity}</p>
                         </div>
                     </div>
-                    <button class="btn btn-primary decrease-quantity" data-product-id="${this.id}">+</button>
-                    <button class="btn btn-primary decrease-quantity" data-product-id="${this.id}">-</button>
+                    <button class="btn btn-primary increase-item-quantity" data-product-id="${this.id}">+</button>
+                    <button class="btn btn-primary decrease-item-quantity" data-product-id="${this.id}">-</button>
                     <button class="btn btn-primary remove-from-cart" data-product-id="${this.id}">Eliminar</button>
                 </div>
             </div>
@@ -46,7 +46,17 @@ export class ShoppingCartItem {
         const removeButton = cartItemContainer.querySelector('.remove-from-cart');
         // Agregamos un EventListener a la variable para que los botones manden a llamar la función para remover
         removeButton.addEventListener('click', () => removeFromCart(this.id));
-
+        
+        // Guardamos los botones para incrementar la cantidad de un producto en una variable
+        const increaseButton = cartItemContainer.querySelector('.increase-item-quantity');
+        // Agregamos un EventListener a la variable para que los botones manden a llamar la función para incrementar
+        increaseButton.addEventListener('click', () => increaseItemQuantity(this.id));
+        
+        // Guardamos los botones para incrementar la cantidad de un producto en una variable
+        const decreaseButton = cartItemContainer.querySelector('.decrease-item-quantity');
+        // Agregamos un EventListener a la variable para que los botones manden a llamar la función para incrementar
+        decreaseButton.addEventListener('click', () => decreaseItemQuantity(this.id));
+        
         // Regresamos el div del producto
         return cartItemContainer;
     }
@@ -147,6 +157,61 @@ function removeFromCart(productId) {
     } catch (error) {
         // En caso de que haya un error al remover el producto del carrito, mostramos un mensaje de error en la consola
         console.error('Error removing item from cart:', error);
+    }
+}
+
+//Función para aumentar la cantidad de un producto en el carrito de compras
+function increaseItemQuantity(productId) {
+    try{
+        //Guardamos el carrito actual del localStorage en una variable, si no existe, asignamos un arreglo vacío
+        let currentCartItems = JSON.parse(localStorage.getItem('shoppingCartLocalStorage') || '[]');
+        //Buscamos el producto en el carrito con el ID especificado
+        const productToUpdate = currentCartItems.find(item => item.id === productId);
+        //Si encontramos el producto, incrementamos su cantidad en 1
+        if (productToUpdate) {
+            productToUpdate.quantity += 1;
+            //Actualizamos el carrito en el localStorage con el carrito actualizado
+            localStorage.setItem('shoppingCartLocalStorage', JSON.stringify(currentCartItems));
+            //Volvemos a cargar el carrito de compras
+            loadShoppingCart(); 
+        } else {
+            // Si no encontramos el producto, mostramos un mensaje de error en la consola
+            console.error('Product not found in the cart'); 
+            alert('Error no se pudo aumentar la cantidad del producto.');
+        }
+    } catch (error) {
+        // En caso de que haya un error al aumentar la cantidad del producto, mostramos un mensaje de error en la consola
+        console.error('Product not found in the cart'); 
+            alert('Error no se pudo aumentar la cantidad del producto.');
+    }
+}
+
+//Función para disminuir la cantidad de un producto en el carrito de compras
+function decreaseItemQuantity(productId) {
+    try{
+        //Guardamos el carrito actual del localStorage en una variable, si no existe, asignamos un arreglo vacío
+        let currentCartItems = JSON.parse(localStorage.getItem('shoppingCartLocalStorage') || '[]');
+        //Buscamos el producto en el carrito con el ID especificado
+        const productToUpdate = currentCartItems.find(item => item.id === productId);
+        //Si encontramos el producto, disminuimos su cantidad en 1
+        if (productToUpdate) {
+            productToUpdate.quantity -= 1;
+            if (productToUpdate.quantity <= 0) {
+                currentCartItems = currentCartItems.filter(item => item.id !== productId);
+            }
+            //Actualizamos el carrito en el localStorage con el carrito actualizado
+            localStorage.setItem('shoppingCartLocalStorage', JSON.stringify(currentCartItems));
+            //Volvemos a cargar el carrito de compras
+            loadShoppingCart(); 
+        } else {
+            // Si no encontramos el producto, mostramos un mensaje de error en la consola
+            console.error('Product not found in the cart'); 
+            alert('Error no se pudo disminuir la cantidad del producto.');
+        }
+    } catch (error) {
+        // En caso de que haya un error al disminuir la cantidad del producto, mostramos un mensaje de error en la consola
+        console.error('Product not found in the cart'); 
+            alert('Error no se pudo disminuir la cantidad del producto.');
     }
 }
 
